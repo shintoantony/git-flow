6bf561b3f7a8cdd9d090b04935f9317535d3a3a3
shinto <shinto1988@gmail.com>
2019-09-27 20:50:42 +0530

    initial commit

---
+++ source-code/base/src/main/java/base/service/Database.java
@@ -0,0 +1,56 @@
+package base.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Database 
+{
+
+	public Connection connectToDB()
+	{
+		Connection con=null;
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://192.168.99.117:3306/sample?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","admin");
+			 
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+	public String getDBStatus()
+	{
+		String status = null;
+		
+		ResultSet r = null;
+		Connection con= null;
+		Statement s= null;
+		try
+		{
+			con=this.connectToDB();
+			s=con.createStatement();
+			String query="select status from STATUS_INFO";
+			r=s.executeQuery(query);
+			while(r.next())
+			{
+				status=r.getString("status");
+			}
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return status;
+		
+	}
+}
